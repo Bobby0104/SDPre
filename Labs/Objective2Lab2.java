622accda0b20de7ae93f11a3fa33feda89ade86e
Bobby <robdiesel87@gmail.com>
2021-07-09 18:45:00 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Robert Jones";
+    int age = 34;
+    String state = "Michigan";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
