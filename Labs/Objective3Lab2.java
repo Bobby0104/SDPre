7de16c8555e0be88caf60cc3905670677bf6872e
Bobby <robdiesel87@gmail.com>
2021-07-10 17:33:57 -0400

    objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = Math.sqrt(10 * 10);
+    double side2 = Math.sqrt(8 * 8);
+    double hypotnuse = Math.sqrt((side1 * side1) + (side2 * side2));
+    System.out.println(hypotnuse);
+  }
+}
