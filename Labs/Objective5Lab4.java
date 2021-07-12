3040a513ccbc8acfd0bf325ccb3edff76601c3a5
Bobby <robdiesel87@gmail.com>
2021-07-12 13:31:51 -0400

    complete objective5lab4

--- Labs/Objective5Lab4.java
+++ Labs/Objective5Lab4.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Objective5Lab4 {
   public static void main(String[] args) {
     Scanner kb = new Scanner(System.in);
-    int num = 6;
+    int num;
     System.out.println("Please enter a number:");
     num = kb.nextInt();
     if(num % 2 == 0) {
