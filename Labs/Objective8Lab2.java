989af9941e3dba344bd2a96e4428d89ea2d43709
Bobby <robdiesel87@gmail.com>
2021-07-13 18:20:30 -0400

    complete objectivelab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,14 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int curSum = 0;
+
+
+    for (int i=1; i<=20; i++){
+      curSum += i;
+    }
+    System.out.println(curSum);
+
+
+
+  }
+}
