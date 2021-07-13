06fa657d817d020f661b67bfd8bbd3f4da7d7e96
Bobby <robdiesel87@gmail.com>
2021-07-13 17:36:33 -0400

    objective7lab4

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -1,13 +1,12 @@
-public class Objective7Lab4 {
-    public static void main(String[] args) {
-        int counter = 1;
-        while(counter <= 20) {
-            if (counter % 2 == 0) {
-                System.out.println(counter + " is even");
-            } else {
-                System.out.println(counter + " is odd");
-            }
-            counter = counter + 1;
-        }
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int counter = 1;
+    int curSum = 0;
+
+    while(counter <= 20){
+      curSum += counter;
+      counter++;
     }
+    System.out.println(curSum);
+  }
 }
